a1e8522b6751400a31e0d4a515ae3c1d9a1a38e1
cfhaak <c.f.haak@posteo.de>
2023-05-26 11:30:30 +0200

    linting, extra logline

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -4,13 +4,13 @@ import requests
 base_row_dump_url = "https://raw.githubusercontent.com/bundesverfassung-oesterreich/bv-entities/main/json_dumps/document.json"
 GOOBI_BASE_URL = "https://viewer.acdh.oeaw.ac.at/viewer/sourcefile?id="
 
+
 def load_metadata_from_dump(dump_url):
     # # loading doc metadata (baserow ump) from url
     get_req = requests.get(dump_url)
     json_data = get_req.json()
     meta_data_objs_by_transkribus_id = {}
     for metadata_dict in json_data.values():
-
         transcribus_col_id = metadata_dict["transkribus_col_id"]
         if not transcribus_col_id:
             # # entrys without collection id are being ignored
@@ -20,7 +20,9 @@ def load_metadata_from_dump(dump_url):
                 # # entrys without doc id get processed
                 if transcribus_col_id not in meta_data_objs_by_transkribus_id:
                     meta_data_objs_by_transkribus_id[transcribus_col_id] = {}
-                meta_data_objs_by_transkribus_id[transcribus_col_id][metadata_dict["bv_id"]] = metadata_dict["doc_title"]
+                meta_data_objs_by_transkribus_id[transcribus_col_id][
+                    metadata_dict["bv_id"]
+                ] = metadata_dict["doc_title"]
     return meta_data_objs_by_transkribus_id
 
 
@@ -36,24 +38,29 @@ def upload_goobidata_via_mets():
         for bv_id, doc_title in items.items():
             # # check if doc_title allready exists in collection
             document_from_title = client.search_for_document(
-                title=doc_title.strip(),
-                col_id=transkribus_collection_id
+                title=doc_title.strip(), col_id=transkribus_collection_id
             )
             if document_from_title:
-                print(f"Document with title '{doc_title}' already exists in collection '{transkribus_collection_id}'. Upload canceled.")
+                print(
+                    f"Document with title '{doc_title}' already exists in collection '{transkribus_collection_id}'. Upload canceled."
+                )
             else:
                 # # upload mets to transkribus
                 print(f"uplaoding '{doc_title}' to '{transkribus_collection_id}'")
                 upload = client.upload_mets_file_from_url(
                     f"{GOOBI_BASE_URL}{bv_id}",
                     transkribus_collection_id,
-                    better_images=True
+                    better_images=True,
                 )
                 if upload:
                     print("upload worked, all good")
                 else:
-                    print(f"upload of '{doc_title}' to '{transkribus_collection_id}' failed.")
+                    print(
+                        f"upload of '{doc_title}' to '{transkribus_collection_id}' failed."
+                    )
+    if len(metadata) == 0:
+        print("Nothing to be done!")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     upload_goobidata_via_mets()
